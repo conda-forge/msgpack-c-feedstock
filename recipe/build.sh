939a032ddbca2b7d5f37c3bf27ad4ba2a1467a8a
Francesco Biscani <bluescarni@gmail.com>
2018-02-06 23:08:11 +0100

    Remove boost support for the moment.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,7 @@ cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DBoost_INCLUDE_DIRS=$PREFIX/include \
-    -DMSGPACK_BOOST_DIR=$PREFIX/include \
     -DMSGPACK_CXX11=YES \
-    -DMSGPACK_BOOST=YES \
     ..
 
 cmake --build .
