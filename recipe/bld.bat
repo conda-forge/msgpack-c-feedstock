bb73de8f5e14097d24e13a43afbe511e306bfc56
Silvio Traversaro <silvio@traversaro.it>
2023-04-21 15:35:52 +0200

    Only build shared libs on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,6 +14,7 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -22,14 +23,4 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 ctest
 ctest --rerun-failed --output-on-failure
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-
-cd "%LIBRARY_PREFIX%/lib"
-ren msgpack-c.lib msgpack-c_static.lib
-ren msgpack-c_import.lib msgpack-c.lib
-
-:: Handle this renaming in the generated CMake configuration
-python "%RECIPE_DIR%\windows_fixup_cmake.py" ^
-    "%LIBRARY_PREFIX%\lib\cmake\msgpack-c\msgpack-c-targets-release.cmake"
 if %errorlevel% neq 0 exit /b %errorlevel%
