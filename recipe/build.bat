d0812fc0b91104ac00621e24317e3091222b9801
Julien Jerphanion <git@jjerphan.xyz>
2026-02-09 16:49:26 +0100

    Add `msgpack-c-static`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.bat
+++ recipe/build.bat
@@ -9,18 +9,35 @@ IF "%ARCH%"=="32" (
     SET EXTRA_COMPILE_FLAGS=/wd4267 /wd4244
 )
 
-cmake ^
-    -GNinja ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=ON ^
-    ..
-if %errorlevel% neq 0 exit /b %errorlevel%
+REM Check which output we're building
+IF "%PKG_NAME%"=="msgpack-c-static" (
+    REM Build static libraries for msgpack-c-static output
+    cmake ^
+        -GNinja ^
+        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+        -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
+        -DCMAKE_BUILD_TYPE=Release ^
+        -DBUILD_SHARED_LIBS=OFF ^
+        ..
+    if %errorlevel% neq 0 exit /b %errorlevel%
 
-ninja install
-if %errorlevel% neq 0 exit /b %errorlevel%
+    ninja install
+    if %errorlevel% neq 0 exit /b %errorlevel%
+) ELSE (
+    REM Build shared libraries for msgpack-c output
+    cmake ^
+        -GNinja ^
+        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+        -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
+        -DCMAKE_BUILD_TYPE=Release ^
+        -DBUILD_SHARED_LIBS=ON ^
+        ..
+    if %errorlevel% neq 0 exit /b %errorlevel%
 
-ctest
-ctest --rerun-failed --output-on-failure
-if %errorlevel% neq 0 exit /b %errorlevel%
+    ninja install
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    ctest
+    ctest --rerun-failed --output-on-failure
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,28 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DBUILD_SHARED_LIBS=ON \
-    ..
+# Check which output we're building
+if [[ $PKG_NAME == "msgpack-c-static" ]]; then
+    # Build static libraries for msgpack-c-static output
+    cmake ${CMAKE_ARGS} \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DBUILD_SHARED_LIBS=OFF \
+        ..
 
-ninja install
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-ctest
+    ninja install
+else
+    # Build shared libraries for msgpack-c output
+    cmake ${CMAKE_ARGS} \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DBUILD_SHARED_LIBS=ON \
+        ..
+
+    ninja install
+    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    ctest
+    fi
 fi
