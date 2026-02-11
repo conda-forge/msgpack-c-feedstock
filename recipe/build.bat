3e0a085ec4d75e928324716f1319ee95d9bec5f7
Julien Jerphanion <git@jjerphan.xyz>
2026-02-11 11:43:11 +0100

    Simplify build script
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    
    Co-authored-by: Silvio Traversaro <silvio@traversaro.it>

--- recipe/build.bat
+++ recipe/build.bat
@@ -11,30 +11,23 @@ IF "%ARCH%"=="32" (
 
 REM Check which output we're building
 IF "%PKG_NAME%"=="libmsgpack-c-static" (
-    cmake ^
-        -GNinja ^
-        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-        -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DBUILD_SHARED_LIBS=OFF ^
-        ..
-    if %errorlevel% neq 0 exit /b %errorlevel%
-    ninja install
-    if %errorlevel% neq 0 exit /b %errorlevel%
+    SET MSGPACK_BUILD_SHARED_LIBS=OFF
 ) ELSE (
-    cmake ^
-        -GNinja ^
-        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-        -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DBUILD_SHARED_LIBS=ON ^
-        ..
-    if %errorlevel% neq 0 exit /b %errorlevel%
-
-    ninja install
-    if %errorlevel% neq 0 exit /b %errorlevel%
-
-    ctest
-    ctest --rerun-failed --output-on-failure
-    if %errorlevel% neq 0 exit /b %errorlevel%
+    SET MSGPACK_BUILD_SHARED_LIBS=ON
 )
+
+cmake ^
+    -GNinja ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=%MSGPACK_BUILD_SHARED_LIBS% ^
+    ..
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+ninja install
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+ctest
+ctest --rerun-failed --output-on-failure
+if %errorlevel% neq 0 exit /b %errorlevel%
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,22 +6,19 @@ cd build
 
 # Check which output we're building
 if [[ $PKG_NAME == "libmsgpack-c-static" ]]; then
-    cmake ${CMAKE_ARGS} \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DBUILD_SHARED_LIBS=OFF \
-        ..
-    ninja install
+    export MSGPACK_BUILD_SHARED_LIBS=OFF
 else
-    cmake ${CMAKE_ARGS} \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DBUILD_SHARED_LIBS=ON \
-        ..
-    ninja install
-    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-        ctest
-    fi
+    export MSGPACK_BUILD_SHARED_LIBS=ON
+fi
+
+cmake ${CMAKE_ARGS} \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_SHARED_LIBS=${MSGPACK_BUILD_SHARED_LIBS} \
+    ..
+
+ninja install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+ctest
 fi
