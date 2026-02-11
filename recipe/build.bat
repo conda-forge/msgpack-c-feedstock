bbd234104604072c05ebff17eb621aa380691ed4
Julien Jerphanion <git@jjerphan.xyz>
2026-02-11 11:56:31 +0100

    Introduce `libmsgpack-c-static` (#54)
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: Silvio Traversaro <silvio@traversaro.it>

--- recipe/build.bat
+++ recipe/build.bat
@@ -9,12 +9,19 @@ IF "%ARCH%"=="32" (
     SET EXTRA_COMPILE_FLAGS=/wd4267 /wd4244
 )
 
+REM Check which output we're building
+IF "%PKG_NAME%"=="libmsgpack-c-static" (
+    SET MSGPACK_BUILD_SHARED_LIBS=OFF
+) ELSE (
+    SET MSGPACK_BUILD_SHARED_LIBS=ON
+)
+
 cmake ^
     -GNinja ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_C_FLAGS="%COMPILE_FLAGS%" ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=ON ^
+    -DBUILD_SHARED_LIBS=%MSGPACK_BUILD_SHARED_LIBS% ^
     ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,18 @@
 mkdir build
 cd build
 
+# Check which output we're building
+if [[ $PKG_NAME == "libmsgpack-c-static" ]]; then
+    export MSGPACK_BUILD_SHARED_LIBS=OFF
+else
+    export MSGPACK_BUILD_SHARED_LIBS=ON
+fi
+
 cmake ${CMAKE_ARGS} \
     -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_SHARED_LIBS=${MSGPACK_BUILD_SHARED_LIBS} \
     ..
 
 ninja install
