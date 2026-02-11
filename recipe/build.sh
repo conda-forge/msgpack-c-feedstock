d2d2b87e630b049e3fd8f308eb227fef2c1ab2f7
Julien Jerphanion <git@jjerphan.xyz>
2026-02-11 10:03:56 +0100

    Introduce `libmsgpack-c-static`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,24 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DBUILD_SHARED_LIBS=ON \
-    ..
-
-ninja install
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-ctest
+# Check which output we're building
+if [[ $PKG_NAME == "libmsgpack-c-static" ]]; then
+    cmake ${CMAKE_ARGS} \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DBUILD_SHARED_LIBS=OFF \
+        ..
+    ninja install
+else
+    cmake ${CMAKE_ARGS} \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DBUILD_SHARED_LIBS=ON \
+        ..
+    ninja install
+    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+        ctest
+    fi
 fi
